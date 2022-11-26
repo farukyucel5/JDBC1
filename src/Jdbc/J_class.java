bf381518a1f73bdf951e0636e325986ce74361d5
Faruk Yucel <farukyucel5@gmail.com>
2022-11-26 20:35:05 +0300

    updating

--- src/Jdbc/J_class.java
+++ src/Jdbc/J_class.java
@@ -1,17 +1,23 @@
 package Jdbc;
 
 import java.awt.image.ShortLookupTable;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class J_class {
 
     public static void main(String[] args) {
         Db_helper helper=new Db_helper();
+        Statement statement=null;
+        ResultSet resultSet;
+
         try {
             Connection connection= helper.getconnection();
-            System.out.println("connected!!");
+            statement=connection.createStatement();
+            resultSet=statement.executeQuery("select title,description,release_year from sakila.film");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("title"));
+            }
         } catch (SQLException e) {
 
             helper.showErrormessage(e);
