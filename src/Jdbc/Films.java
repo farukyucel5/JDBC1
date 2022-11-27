616c7afe4e8fe96068d0ba6a9e48cddb48e7cb6b
Faruk Yucel <farukyucel5@gmail.com>
2022-11-27 23:27:06 +0300

    uptading

--- src/Jdbc/Films.java
+++ src/Jdbc/Films.java
@@ -4,10 +4,13 @@ public class Films {
     private String title;
     private String description;
     private String release_year;
-    Films(String title, String description, String release_year) {
+
+    private String language_id;
+    Films(String title, String description, String release_year,String language_id) {
         this.title=title;
         this.description=description;
         this.release_year=release_year;
+        this.language_id=language_id;
     }
 
 
--- src/Jdbc/J_class.java
+++ src/Jdbc/J_class.java
@@ -8,20 +8,52 @@ public class J_class {
 
     public static void main(String[] args) {
         Db_helper helper=new Db_helper();
+        PreparedStatement statement=null;
+
+        selectdemo();//eklenmeden önce
+
+
+        try {
+            Connection connection= helper.getconnection();
+            String sql="insert into sakila.film (title,description,release_year,language_id) values (?,?,?,?)";
+            statement=connection.prepareStatement(sql);
+            statement.setString(1,"Pieta");
+            statement.setString(2,"revenge");
+            statement.setString(3,"2017");
+            statement.setString(4,"2");
+            statement.executeUpdate();
+            System.out.println("it's just been registered");
+
+
+
+
+        } catch (SQLException e) {
+
+            helper.showErrormessage(e);
+        }
+
+        selectdemo();//eklenmeden sonra
+
+    }
+
+   public static void selectdemo()
+    {
+        Db_helper helper=new Db_helper();
         Statement statement=null;
         ResultSet resultSet;
 
         try {
             Connection connection= helper.getconnection();
             statement=connection.createStatement();
-            resultSet=statement.executeQuery("select title,description,release_year from sakila.film");
+            resultSet=statement.executeQuery("select title,description,release_year,language_id from sakila.film");
 
             ArrayList<Films> films=new ArrayList<>();
 
             while (resultSet.next()) {
                 Films film_object=new Films(resultSet.getString("title"),
                         resultSet.getString("description"),
-                        resultSet.getString("release_year"));
+                        resultSet.getString("release_year"),
+                        resultSet.getString("language_id"));
                 films.add(film_object);
 
             }
